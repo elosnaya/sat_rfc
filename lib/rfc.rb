a1b363ef2730575bb987cfbcc9c2538c4f604af9
Albert Osbal <luisosnet@gmail.com>
2026-06-30 11:43:25 -0600

    initial commit

---
+++ lib/rfc.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "rfc/version"
+
+module Rfc
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/rfc/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Rfc
+  VERSION = "0.1.0"
+end
